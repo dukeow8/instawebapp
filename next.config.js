8e7aa5c198139511c979f3efcd1e8abd6fe9d1e6
Ndumiso Banda <67536043+dukeow8@users.noreply.github.com>
2021-10-20 21:35:51 -0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    target: 'serverless',
     serverRuntimeConfig: {
         secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
     },
